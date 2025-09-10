4ac5d2cbb739f67637bd676f9e95549b6d42b830
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-09-10 08:26:06 -0300

    consulting

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -3,7 +3,18 @@ import { prisma } from "@/prisma"
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json()
+    const questions = await prisma.questions.findMany({
+      where: {
+        title: {
+          contains: request.query.title?.toString().trim(),
+          mode: "insensitive",
+        },
+      },
+      orderBy: {
+          title: "asc"
+        },
+    })
+    return response.json({questions})
   }
 
   async create(request: Request, response: Response) {
